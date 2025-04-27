c107848f465f43adeb025cc9869df40040feee96
Angeline Apriliana <angeline.apriliana@mhs.itenas.ac.id>
2025-04-27 22:51:01 +0700

    TIGA

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ app = Flask(__name__)
 
 # Database connection (PostgreSQL)
 db = pymysql.connect(
-    host='ecom-db1.chm448q8s314.ap-southeast-1.rds.amazonaws.com',
+    host='ecom1-db.chm448q8s314.ap-southeast-1.rds.amazonaws.com',
     user='admin117',
     password='angeline117!',
-    dbname='ecom1-db'
+    database='ecom1-db'
 )
 
 @app.route('/')
